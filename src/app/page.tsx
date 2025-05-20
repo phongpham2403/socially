30b615b53d6d26600b3ae4cf41b6f8d276fc20c2
phongpham240300 <phongpham240300@gmail.com>
2025-05-20 08:48:58 +0700

    fix sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,27 @@
+import { getDbUserId } from "@/actions/user.action";
+import CreatePost from "@/components/CreatePost";
+import WhoToFollow from "@/components/WhoToFollow";
+import { currentUser } from "@clerk/nextjs/server";
 import Image from "next/image";
 
-export default function Home() {
-  return <>Hello</>;
+export default async function Home() {
+  const user = await currentUser();
+  const dbUserId = await getDbUserId();
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+      <div className="lg:col-span-6">
+        {user ? <CreatePost /> : null}
+
+        <div className="space-y-6">
+          {/* {posts.map((post) => (
+            <PostCard key={post.id} post={post} dbUserId={dbUserId} />
+          ))} */}
+        </div>
+      </div>
+
+      <div className="hidden lg:block lg:col-span-4 sticky top-20">
+        <WhoToFollow />
+      </div>
+    </div>
+  );
 }
